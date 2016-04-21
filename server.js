3ed17a4adbe7617db1c7a27dd9c621f88ef0f8db
Tyler St. Onge <tylertstonge@gmail.com>
2016-04-21 13:21:24 -0400

    tweaks

--- server.js
+++ server.js
@@ -14,15 +14,20 @@ if (!exists) {
 
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
+const PORT = 8080;
 
+/**
+ * Create the database
+ */
 db.serialize(function() {
 	if(!exists) {
 		db.run("CREATE TABLE users ( id integer primary key asc, userid integer not null, totalratio float not null)");
 	}
 });
 
-const PORT = 8080;
-
+/**
+ * Dispatcher method to determine which url is being requested.
+ */
 function handleRequest(req, res) {
 	try {
 		console.log(req.url);
@@ -32,6 +37,9 @@ function handleRequest(req, res) {
 	}
 }
 
+/**
+ * Main page is requested, send them a cool message
+ */
 dispatcher.onGet("/", function(req, res) {
 	res.writeHead(200, {'Content-Type': 'application/json'});
 	res.end("PersonalMoneyManager API");
@@ -59,17 +67,42 @@ dispatcher.onPost("/", function(req, res) {
 	res.end(req.body);
 });
 
+var generateId = function() {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var userid = '';
+	var length = 64;
+	for (var i = length; i > 0; i--) {
+		userid += chars[Math.round(Math.random() * (chars.length - 1))];
+	}
+	return userid;
+}
+
+/**
+ * Generate a userid for a new user, this will uniquely identify them in our database. 
+ * If generation fails, userid will return null.
+ */
+dispatcher.onPost("/newuser", function(req, res) {
+	db.serialize(function() {
+		db.all("SELECT userid FROM users", function(err, rows) {
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			var userid = generateId();
+			for (var i = 0; i < rows.length; i++) {
+				if (rows[i].userid == userid) {
+					res.end(JSON.stringify({"userid": null}));
+					return;
+				}	
+			}
+			res.end(JSON.stringify({"userid": userid}));
+		});
+	});
+});
+
 /**
  * Calculate and recieve the ratio of overall purchases of users
  *
  * totalratio - the ratio of purchases / household size
  */
 dispatcher.onPost("/gimme/totalratio", function(req, res) {
-	var respond = function(data) {
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify(data));
-	}
-
 	db.serialize(function() {
 		db.all("SELECT totalratio FROM users", function(err, rows) {
 			var data = {
@@ -80,15 +113,14 @@ dispatcher.onPost("/gimme/totalratio", function(req, res) {
 				data.totalratio += rows[i].totalratio;
 			}
 
-			console.log("Total is: " + data.totalratio);
 			data.totalratio = data.totalratio / rows.length;
-			respond(data);
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(data));
 		});
 	});
 });
 
 var server = http.createServer(handleRequest);
-
 server.listen(PORT, function() {
-	console.log("server listening on : http://localhost:%s", PORT);
+	console.log("server listening on: http://localhost:%s", PORT);
 });
